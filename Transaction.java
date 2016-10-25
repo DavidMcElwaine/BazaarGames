050b70d6106f71a431d9ce7cdce0c2d2fddd0c49
DavidMcElwaine <DavidMcElwaine@users.noreply.github.com>
2016-10-25 14:18:24 +0100

    Add files via upload

---
+++ Transaction.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+public class Transaction{
+		
+		public void UpdateCustomer(String username, String game) throws Exception{
+			
+			File originalLibrary = new File("library.csv");
+			File newLibrary = new File("newLibrary.csv");
+			Scanner in = new Scanner(originalLibrary);
+			FileWriter writer = new FileWriter(newLibrary);
+			String updatedLibrary = new String("");
+			
+			while(in.hasNext()){
+				String currentLine = in.nextLine();
+				String[] temp = currentLine.split(",");
+				if((username).matches(temp[0])) updatedLibrary = updatedLibrary + currentLine + game + "\n" ;
+				else updatedLibrary = updatedLibrary + currentLine + "\n";
+			}
+			
+			writer.write(updatedLibrary);
+			writer.flush();
+			writer.close();
+			writer.close();
+		}	
+}
