08d8e2e08b569594d3158881d449b7fdc7612aef
DavidMcElwaine <DavidMcElwaine@users.noreply.github.com>
2016-11-02 17:46:12 +0000

    Update Product.java

--- Product/Product.java
+++ Product/Product.java
@@ -1,4 +1,10 @@
 public interface Product{
 
 	public void buy(String username, String title);
+	
+	public String getTitle();
+	public double getPrice();
+	public int getSales();
+	public int getYear();
+	public String getGenre();
 }
