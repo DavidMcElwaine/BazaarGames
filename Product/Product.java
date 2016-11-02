05439639e84a1612eab4d68cf8bfe08bb562f8e2
DavidMcElwaine <DavidMcElwaine@users.noreply.github.com>
2016-11-02 17:31:15 +0000

    Update Product.java

--- Product/Product.java
+++ Product/Product.java
@@ -1,10 +1,4 @@
 public interface Product{
 
-	private String title;
-	private double price;
-	private int sales;
-	private int year;
-	private String genre;
-
 	public buy(String username, String title);
 }
