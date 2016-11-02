532b179426ee38530589816bee1cf20fcdb05fb7
DavidMcElwaine <DavidMcElwaine@users.noreply.github.com>
2016-11-02 16:59:00 +0000

    Create Product.java

---
+++ Product/Product.java
@@ -0,0 +1,10 @@
+public interface Product{
+
+	private String title;
+	private double price;
+	private int sales;
+	private int year;
+	private String genre;
+
+	public buy(String username, String title);
+}
