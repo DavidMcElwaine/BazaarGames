9493f0b95d86177bebb6da126f5a2ca6baae1883
breanainnc <breanainnc@live.ie>
2016-11-06 18:49:19 +0000

    Add files via upload

--- User.java
+++ User.java
@@ -5,25 +5,38 @@ public class User{
 	private int numFriends;
 	private int spendings;
 	private double balance;
-	private ArrayList<String> library;
+	private ArrayList<Product> library;
 	private Rank rank;
 	
 	public User(String username){
+		this.library = new ArrayList<String>();
 		this.username = username;
 		this.numFriends = 0;
 		this.numReviews = 0;
-	    	this.spendings = 0;
+	    this.spendings = 0;
 		this.balance = 0;
 		this.library = new ArrayList<String>();
 	}
-	public User(String username, int r, int f,int s, double b){
+	public User(String username, int r, int f,int s, double b,ArrayList<String> library){
 		this.username = username;
 		this.numReviews = r;
 		this.numFriends = f;
 		this.spendings = s;
 		this.balance = b;
+		this.library = library;
 	}
 	
+		public static User getUser(String username, int r, int f,int s, double b){
+		ArrayList<String> library = LibraryCSVReader.getLibrary(username);
+		return new User(username,r,f,s,b,library);
+		
+	}
+	
+	public static User getUser(String username){
+		//braryCSVReader.getLibrary(username);
+		return new User(username);
+		
+	}
 	
 	ArrayList<String> getLibrary(){
 		return library;
@@ -47,3 +60,5 @@ public class User{
 	}
 
 }
+
+
