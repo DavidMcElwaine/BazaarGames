8b53740ff534442d7b29bf5b284eee8414711f37
EoinHanan <14176459@studentmail.ul.ie>
2016-11-08 12:39:30 +0000

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 public class User{
 	private String username;
@@ -14,9 +15,17 @@ public class User{
 		this.numReviews = 0;
 	        this.spendings = 0;
 		this.balance = 0;
-		this.library = new ArrayList<String>();
+		this.library = new ArrayList<Product>();
 	}
-	public User(String username, int r, int f,int s, double b,ArrayList<String> library){
+        public User(String username, int r, int f,int s, double b){
+		this.username = username;
+		this.numReviews = r;
+		this.numFriends = f;
+		this.spendings = s;
+		this.balance = b;
+                this.library = new ArrayList<Product>();
+	}
+	public User(String username, int r, int f,int s, double b,ArrayList<Product> library){
 		this.username = username;
 		this.numReviews = r;
 		this.numFriends = f;
@@ -25,8 +34,11 @@ public class User{
 		this.library = library;
 	}
 	
-		public static User getUser(String username, int r, int f,int s, double b){
-		ArrayList<String> library = LibraryCSVReader.getLibrary(username);
+		public static User getUser(String username, int r, int f,int s, double b) throws FileNotFoundException{
+		ArrayList<Product> library = LibraryCSVReader.getLibrary(username); 
+                
+                
+                
 		return new User(username,r,f,s,b,library);
 		
 	}
@@ -37,7 +49,7 @@ public class User{
 		
 	}
 	
-	ArrayList<String> getLibrary(){
+	ArrayList<Product> getLibrary(){
 		return library;
 	}
 	
@@ -57,7 +69,7 @@ public class User{
 	int getSpendings(){
 		return spendings;
 	}
-
+        double getBalance(){
+           return balance; 
+        }
 }
-
-
