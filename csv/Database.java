d6ea2b56b5fe26e660b0b68c1d855738a4e6ef3a
EoinHanan <14176459@studentmail.ul.ie>
2016-11-08 12:59:05 +0000

    Update Database.java

--- csv/Database.java
+++ csv/Database.java
@@ -7,6 +7,7 @@ package bazaar;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -19,6 +20,7 @@ public class Database {
     public Database()throws IOException
     {
         allProducts = new ArrayList<Product>();
+        allUsers = new ArrayList<User>();
         readGames();
         readUsers();
     }
@@ -38,6 +40,7 @@ public class Database {
                 game = new Game(lineElements[0],Double.parseDouble(lineElements[1]),Integer.parseInt(lineElements[2]),Integer.parseInt(lineElements[3]),lineElements[4],Integer.parseInt(lineElements[5]),lineElements[6],Boolean.parseBoolean(lineElements[7]));
                 allProducts.add(game);
             }
+            in.close();
         }
         else {
             System.out.print("No games.txt file");
@@ -45,10 +48,48 @@ public class Database {
     }
     public void readUsers() throws IOException
     {
-        
+        User user;
+        File file = new File("users.txt");
+        if (file.exists()){
+            in = new Scanner(file);
+            System.out.println("user.txt Found");
+            String line;
+            String lineElements[];
+            while(in.hasNext()){
+                line = in.nextLine();
+                System.out.println(line);
+                lineElements = line.split(",");
+                user = new User(lineElements[0],Integer.parseInt(lineElements[1]),Integer.parseInt(lineElements[2]),Integer.parseInt(lineElements[3]),Double.parseDouble(lineElements[4]));
+                allUsers.add(user);
+            }
+            in.close();
+        }
+        else {
+            System.out.print("No users.txt file");
+        }
     }
-    public ArrayList <Product> giveList()
+    public ArrayList <Product> giveProductList()
     {
         return allProducts;
     }
+    public ArrayList <User> giveUserList()
+    {
+        return allUsers;
+    }
+    public void addUser(User user)throws IOException
+    {
+        File file = new File("users.txt");
+        
+        if (file.exists()){
+            in = new Scanner(file);
+            while(in.hasNext())
+            {
+            }
+        }
+        in.close();
+        PrintWriter out = new PrintWriter(file);
+        out.print("" + user.getName() + "," + user.getNumFriends() + "," + user.getNumReviews() +"," + user.getSpendings() + "," + user.getBalance());
+        
+    }
 }
+
